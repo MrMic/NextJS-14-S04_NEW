38b936b9482d3be3ae53b9303b59fa0cf3a4c11d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-09 12:46:43 +0200

    💫 feat: Configure Catch-All Routes

--- app/news/page.js
+++ app/news/page.js
@@ -1,11 +1,11 @@
-import { DUMMY_NEWS } from '@/dummy-news';
-import NewsList from '@/components/news-list';
+import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
-  return (
-    <>
-      <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
-    </>
-  );
+return (
+<>
+<h1>News Page</h1>
+<NewsList news={DUMMY_NEWS} />
+</>
+);
 }
